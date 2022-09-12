5fef303d96e5c691d455170a4348bbd485466cbf
Gracy98 <olasomigrace@gmail.com>
2022-09-12 14:38:36 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints a text
+ * main - prints text
  *
- * Result: 1 (Success)
+ * Result: 1 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" -Dora Korpar,2015-10-19\n", 59);
 	return (1);
 }
